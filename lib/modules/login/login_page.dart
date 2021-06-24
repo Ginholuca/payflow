8c7177db55c1079771fe6c3a716613cfe7fe0c2c
Giovanni De Luca <ginholucacorrea@icloud.com>
2021-06-24 04:24:44 -0300

    LoginPage

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -32,12 +34,34 @@ class _LoginPageState extends State<LoginPage> {
                 child: Image.asset(
                   AppImages.person,
                   width: 208,
-                  height: 373,
+                  height: 300,
                 )),
             Positioned(
-              bottom: 50,
+              bottom: size.height * 0.05,
+              left: 0,
+              right: 0,
               child: Column(
-                children: [Image.asset(AppImages.logomini)],
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () {},
+                    ),
+                  )
+                ],
               ),
             )
           ],
