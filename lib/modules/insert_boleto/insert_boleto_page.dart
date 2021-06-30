f891ccd83719a88c487d017236591efb5538b12c
Giovanni De Luca <ginholucacorrea@icloud.com>
2021-06-30 02:25:59 -0300

    boletomodel

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text2/flutter_masked_text2.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/input_text/input_text_widget.dart';
@@ -15,6 +16,8 @@ class InsertBoletoPage extends StatefulWidget {
 }
 
 class _InsertBoletoPageState extends State<InsertBoletoPage> {
+  final controller = InsertBoletoController();
+
   final moneyInputTextController =
       MoneyMaskedTextController(leftSymbol: "R\$", decimalSeparator: ",");
 
@@ -39,48 +42,71 @@ class _InsertBoletoPageState extends State<InsertBoletoPage> {
             color: AppColors.input,
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 93,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 93,
+                  ),
+                  child: Text(
+                    "Preecha os dados do boleto",
+                    style: TextStyles.titleBoldHeading,
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                child: Text(
-                  "Preecha os dados do boleto",
-                  style: TextStyles.titleBoldHeading,
-                  textAlign: TextAlign.center,
+                SizedBox(
+                  height: 24,
                 ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              InputTextWidget(
-                label: "Nome do boleto",
-                icon: Icons.description_outlined,
-                onChanged: (value) {},
-              ),
-              InputTextWidget(
-                controller: dueDateInputTextController,
-                label: "Vencimento",
-                icon: FontAwesomeIcons.timesCircle,
-                onChanged: (value) {},
-              ),
-              InputTextWidget(
-                controller: moneyInputTextController,
-                label: "Valor",
-                icon: FontAwesomeIcons.wallet,
-                onChanged: (value) {},
-              ),
-              InputTextWidget(
-                controller: barcodeInputTextController,
-                label: "Código",
-                icon: FontAwesomeIcons.barcode,
-                onChanged: (value) {},
-              )
-            ],
+                Form(
+                  key: controller.formKey,
+                  child: Column(
+                    children: [
+                      InputTextWidget(
+                        label: "Nome do boleto",
+                        icon: Icons.description_outlined,
+                        validator: controller.validateName,
+                        onChanged: (value) {
+                          controller.onChange(name: value);
+                        },
+                      ),
+                      InputTextWidget(
+                        controller: dueDateInputTextController,
+                        validator: controller.validateVencimento,
+                        label: "Vencimento",
+                        icon: FontAwesomeIcons.timesCircle,
+                        onChanged: (value) {
+                          controller.onChange(dueDate: value);
+                        },
+                      ),
+                      InputTextWidget(
+                        controller: moneyInputTextController,
+                        label: "Valor",
+                        validator: (_) => controller.validateValor(
+                            moneyInputTextController.numberValue),
+                        icon: FontAwesomeIcons.wallet,
+                        onChanged: (value) {
+                          controller.onChange(
+                              value: moneyInputTextController.numberValue);
+                        },
+                      ),
+                      InputTextWidget(
+                        controller: barcodeInputTextController,
+                        label: "Código",
+                        validator: controller.validateCodigo,
+                        icon: FontAwesomeIcons.barcode,
+                        onChanged: (value) {
+                          controller.onChange(barcode: value);
+                        },
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: SetLabelButtons(
---
+++ lib/shared/models/boleto_model.dart
@@ -0,0 +1,6 @@
+class BoletoModel {
+  final String? name;
+  final String? dueDate;
+  final double value;
+  final String? barcode;
+}
